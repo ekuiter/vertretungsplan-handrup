9e57e059d2c705df5894253c7e3aa5c10fd64c35
Elias Kuiter <info@elias-kuiter.de>
2014-09-26 22:13:19 +0200

    Fix: "Internetverbindung getrennt" beim Starten

--- client/helpers/main.js
+++ client/helpers/main.js
@@ -1,9 +1,9 @@
-Template.layout.disconnected = function() {
-  return !Meteor.status().connected;
-};
-
 Template.layout.events({
   "click .disconnected button": function() {
     Meteor.reconnect();
   }
 });
+
+Template.disconnected.isDisconnected = function() {
+  return !Meteor.status().connected;
+};
