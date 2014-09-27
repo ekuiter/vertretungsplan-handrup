07a156d7828477e833eb478cdaa42b82ff42db02
Elias Kuiter <info@elias-kuiter.de>
2014-09-27 19:53:00 +0200

    Cordova: Back Button

--- client/startup.js
+++ client/startup.js
@@ -4,4 +4,17 @@ Meteor.startup(function() {
   Meteor.connection.onReconnect = function() {
     Users.tryToLoginWithMoodleSession();
   };
+
+  if (Meteor.isCordova) {
+    document.addEventListener("deviceready", function() {
+      navigator.splashscreen.hide();
+
+      document.addEventListener("backbutton", function() {
+        if (Router.current().route.name === "today")
+          navigator.app.exitApp();
+        else
+          navigator.app.backHistory();
+      }, false);
+    }, false);
+  }
 });
